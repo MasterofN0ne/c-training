30073e47c40032599ecf3a6f2a8480fdd66141eb
Serhat Karaman <90270116+MasterofN0ne@users.noreply.github.com>
2022-04-26 00:30:24 +0300

    updated until q6

--- main.c
+++ main.c
@@ -243,7 +243,7 @@ void q1_j(){
 
     /*
         ---------OUTPUT----------
-        
+
         0, 0, 15, res = 15 -> starting point
         1, 2, 12, res = 15 -> true
         2, 3, 7, res = 12 -> true
@@ -253,9 +253,199 @@ void q1_j(){
 
 // -------------------------------------------------------
 
+
+void q2_a(){
+    /* 
+        Rewrite using switch construction
+        if (a=='a') x++;
+        else if (a == 'b') x--;
+        else y++;
+    */
+
+    char a;
+    int x, y;
+
+    switch(a){
+        case 'a': x++; break;
+        case 'b': x--; break;
+        default:
+            y++;
+    }
+}
+
+// -------------------------------------------------------
+
+void q2_b(){
+    /*
+        Rewrite without continue
+        while (1){
+            if (scanf("%c", &a), a=='1') continue;
+            x++;
+        }
+    */
+
+   while (1){
+       if(scanf("%c", &a), a != '1') x++;
+   }
+}
+
+// -------------------------------------------------------
+
+void q2_c(){
+    /*
+        Rewrite using for instead of while
+
+        int year = 1900; int sum = 0;
+
+        while (year<=4002){
+            sum += year;
+            printf("/nYear=%d",year);
+            year += sum;
+        }
+    */
+
+   // Well if we dont use while there is one option which is a for loop
+    int sum = 0;
+    for (int year = 1900; year <= 4002; sum += year, year += sum)
+        printf("/nYear=%d",year);
+}
+
+// -------------------------------------------------------
+
+void q2_d(){
+    /*
+        Rewrite
+            if(x<y) a = x + 1;
+            else a = y + 1;
+    */
+
+    a = x<y ? x + 1: y + 1;
+}
+
+// -------------------------------------------------------
+
+void q3(){
+    int i;
+    for(i=0;i<5;i++){
+        switch(i)
+            {	case 0:  printf("*****\n"); break;
+
+                case 4:  printf("*****\n"); break;
+
+                default: printf("*   *\n");
+            }
+    }
+    /*
+        *****
+        *   *
+        *   * -> Aim is to display that
+        *   *
+        ***** 
+
+        0 and 4 are the start and the end of this 
+        shape
+
+        in other indexes we will just print '*   *' that
+    */
+}
+
+// -------------------------------------------------------
+
+void dispNumbers(int n){
+    int i;
+    for (i = n; i > 0; i--){
+        printf("%d", i);
+    }
+    printf("\n");
+    /* 
+        lets say n = 5
+        then this function returns '54321'
+    */
+}
+
+void q5(){
+    for (int i = 1; i < 5; i++) dispNumbers(i);
+    for (int j = 3; j > 0; j--) dispNumbers(j);
+
+    /*
+        To get reverse numbers 
+        we give numbers to dispNumbers function 
+        then we get the thing below
+
+        1
+        21
+        321
+        4321
+        321
+        21
+        1
+    */
+}
+
+// -------------------------------------------------------
+// GPA CALCULATION
+/*
+    Each student takes 5 courses
+    Course is described as Course code, Letter Grade, Course Credit
+    
+    Student:
+        Student Number(long integer)
+        Name of the student(string)
+    
+    Course:
+        Course Code(string of 7 chars)
+        Letter Grade (one of the chars 'A', 'B', 'C', 'D', 'F',)
+        Course Credit (integer)
+*/
+
+void calculateGPA(){
+    char[7] courseCode;
+    char letterGrade;
+    int courseCredit;
+
+    long studentNumber;
+    char[20] studentName;
+
+    int sumOfCredits = 0;
+    int sumOfPoints = 0;
+
+    printf("Enter your name and student number: ");
+    scanf("%c%ld", &studentName, &studentNumber);
+
+    for (int i = 0; i <= 5; ++i){
+        printf("Dear %s please enter courseCode, letterGrade and courseCredit: ", studentName);
+        scanf("%c%c%d", &courseCode, &letterGrade, &courseCredit);
+
+        switch(letterGrade){
+            case 'A':
+                sumOfPoints += 4 * courseCredit;
+                break;
+            case 'B':
+                sumOfPoints += 3 * courseCredit;
+                break;
+            case 'C':
+                sumOfPoints += 2 * courseCredit;
+                break;
+            case 'D':
+                sumOfPoints += 1 * courseCredit;
+                break;
+            case 'F':
+                sumOfPoints += 0 * courseCredit;
+                break;
+            default:
+                printf("Something went wrong\n");
+        }
+        sumOfCredits += courseCredit;
+    }
+
+    double gpa = sumOfPoints / sumOfCredits;
+    printf("Student with the name %s and %ld student number has the gpa of %lf\n", studentName, studentNumber, gpa);
+
+}
+
 int main(){
         
-    q1_i();
+    calculateGPA();
         
     return 0;
 }
